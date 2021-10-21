67b3aa80d154e13f8a817655842aa70b70d9bfb4
Nam Nikon <trungnam47.mc@gmail.com>
2021-10-21 21:39:36 +0700

    Week_02

--- index.php
+++ index.php
@@ -4,9 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bài tập HTML nâng cao</title>
 </head>
 <body>
-    
+    <h1>Danh sách bài tập HTML nâng cao</h1>
+    <ul>
+        <li>
+            <a href="18600177_01.html">Bài tập 1</a>
+        </li>
+        <li>
+            <a href="18600177_02.html">Bài tập 2</a>
+        </li>
+        <li>
+            <a href="18600177_03.html">Bài tập 3</a>
+        </li>
+        <li>
+            <a href="18600177_04.html">Bài tập 4</a>
+        </li>
+        <li>
+            <a href="18600177_05.html">Bài tập 5</a>
+        </li>
+        <li>
+            <a href="18600177_06.html">Bài tập 6</a>
+        </li>
+        <li>
+            <a href="18600177_07.html">Bài tập 7</a>
+        </li>
+        <li>
+            <a href="18600177_08.html">Bài tập 8</a>
+        </li>
+        <li>
+            <a href="18600177_09.html">Bài tập 9</a>
+        </li>
+        <li>
+            <a href="18600177_10.html">Bài tập 10</a>
+        </li>
+        <li>
+            <a href="18600177_11.html">Bài tập 11</a>
+        </li>
+        <li>
+            <a href="18600177_12.html">Bài tập 12</a>
+        </li>
+    </ul>
 </body>
 </html>
